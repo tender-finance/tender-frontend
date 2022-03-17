89a623308aa20ed2c1384fcd5c64c047045c810c
Ryan LaBouve <imryanlabouve@gmail.com>
2022-03-17 12:57:45 -0500

    Stubs out the two modals

--- app/components/borrow-flow.tsx
+++ app/components/borrow-flow.tsx
@@ -1,3 +1,31 @@
-export default function BorrowFlow() {
-  return <div>Borrow Flow</div>;
+import { SwapRow } from "~/types/global";
+interface Props {
+  closeModal: Function;
+  row: SwapRow;
+}
+export default function BorrowFlow({ closeModal, row }: Props) {
+  return (
+    <div>
+      <div className="py-8" style={{ backgroundColor: "#23262B" }}>
+        <div className="float-right">
+          <button
+            onClick={() => closeModal()}
+            className="text-4xl rotate-45 text-gray-400 mr-8"
+          >
+            +
+          </button>
+        </div>
+
+        <div className="flex align-middle justify-center items-center">
+          <div className="mr-4">
+            <img src={row.icon} />
+          </div>
+          <div>Borrow {row.name}</div>
+        </div>
+      </div>
+      <div className="p-6" style={{ background: "#1C1E22" }}>
+        DEFI!
+      </div>
+    </div>
+  );
 }
