3f78bc65e7920744c7e818154067749ed816a587
Ryan LaBouve <imryanlabouve@gmail.com>
2022-03-17 17:29:51 -0500

    Toggle buttons in modal

--- app/components/borrow-flow.tsx
+++ app/components/borrow-flow.tsx
@@ -1,10 +1,14 @@
 import { SwapRow } from "~/types/global";
+
+import { useState } from "react";
+
 interface Props {
   closeModal: Function;
   row: SwapRow;
 }
 export default function BorrowFlow({ closeModal, row }: Props) {
-  return (
+  let [isBorrowing, setIsBorrowing] = useState<boolean>(true);
+  return isBorrowing ? (
     <div>
       <div className="py-8" style={{ backgroundColor: "#23262B" }}>
         <div className="float-right">
@@ -15,16 +19,67 @@ export default function BorrowFlow({ closeModal, row }: Props) {
             +
           </button>
         </div>
-
         <div className="flex align-middle justify-center items-center">
           <div className="mr-4">
             <img src={row.icon} />
           </div>
-          <div>Borrow {row.name}</div>
+          <div>Deposit {row.name}</div>
         </div>
+      </div>
+      <div className="flex">
+        <button
+          className="flex-grow py-3 text-brand-green border-b-2 border-b-brand-green"
+          onClick={() => setIsBorrowing(true)}
+        >
+          Borrow
+        </button>
+        <button
+          className="flex-grow py-3"
+          onClick={() => setIsBorrowing(false)}
+        >
+          Repay
+        </button>
       </div>
       <div className="p-6" style={{ background: "#1C1E22" }}>
         DEFI!
+      </div>
+    </div>
+  ) : (
+    <div>
+      <div>
+        <div className="py-8" style={{ backgroundColor: "#23262B" }}>
+          <div className="float-right">
+            <button
+              onClick={() => closeModal()}
+              className="text-4xl rotate-45 text-gray-400 mr-8"
+            >
+              +
+            </button>
+          </div>
+          <div className="flex align-middle justify-center items-center">
+            <div className="mr-4">
+              <img src={row.icon} />
+            </div>
+            <div>Deposit {row.name}</div>
+          </div>
+        </div>
+        <div className="flex">
+          <button
+            className="flex-grow py-3"
+            onClick={() => setIsBorrowing(true)}
+          >
+            Borrow
+          </button>
+          <button
+            className="flex-grow py-3 text-brand-green border-b-2 border-b-brand-green"
+            onClick={() => setIsBorrowing(false)}
+          >
+            Repay
+          </button>
+        </div>
+        <div className="p-6" style={{ background: "#1C1E22" }}>
+          DEFI!
+        </div>
       </div>
     </div>
   );
--- app/components/deposit-flow.tsx
+++ app/components/deposit-flow.tsx
@@ -1,10 +1,12 @@
 import { SwapRow } from "~/types/global";
+import { useState } from "react";
 interface Props {
   closeModal: Function;
   row: SwapRow;
 }
 export default function DepositFlow({ closeModal, row }: Props) {
-  return (
+  let [isSupplying, setIsSupplying] = useState<boolean>(true);
+  return isSupplying ? (
     <div>
       <div className="py-8" style={{ backgroundColor: "#23262B" }}>
         <div className="float-right">
@@ -15,7 +17,6 @@ export default function DepositFlow({ closeModal, row }: Props) {
             +
           </button>
         </div>
-
         <div className="flex align-middle justify-center items-center">
           <div className="mr-4">
             <img src={row.icon} />
@@ -23,8 +24,60 @@ export default function DepositFlow({ closeModal, row }: Props) {
           <div>Deposit {row.name}</div>
         </div>
       </div>
+      <div className="flex">
+        <button
+          className="flex-grow py-3 text-brand-green border-b-2 border-b-brand-green"
+          onClick={() => setIsSupplying(true)}
+        >
+          Supply
+        </button>
+        <button
+          className="flex-grow py-3"
+          onClick={() => setIsSupplying(false)}
+        >
+          Widthdraw
+        </button>
+      </div>
       <div className="p-6" style={{ background: "#1C1E22" }}>
         DEFI!
+      </div>
+    </div>
+  ) : (
+    <div>
+      <div>
+        <div className="py-8" style={{ backgroundColor: "#23262B" }}>
+          <div className="float-right">
+            <button
+              onClick={() => closeModal()}
+              className="text-4xl rotate-45 text-gray-400 mr-8"
+            >
+              +
+            </button>
+          </div>
+          <div className="flex align-middle justify-center items-center">
+            <div className="mr-4">
+              <img src={row.icon} />
+            </div>
+            <div>Deposit {row.name}</div>
+          </div>
+        </div>
+        <div className="flex">
+          <button
+            className="flex-grow py-3"
+            onClick={() => setIsSupplying(true)}
+          >
+            Supply
+          </button>
+          <button
+            className="flex-grow py-3 text-brand-green border-b-2 border-b-brand-green"
+            onClick={() => setIsSupplying(false)}
+          >
+            Widthdraw
+          </button>
+        </div>
+        <div className="p-6" style={{ background: "#1C1E22" }}>
+          DEFI!
+        </div>
       </div>
     </div>
   );
