bfe7e3121b5d327c543f19813cf7b96f6a8d210d
Ryan LaBouve <imryanlabouve@gmail.com>
2022-03-18 14:24:29 -0500

    WIP

--- app/components/deposit-flow.tsx
+++ app/components/deposit-flow.tsx
@@ -24,7 +24,7 @@ export default function DepositFlow({ closeModal, row }: Props) {
           <div>Deposit {row.name}</div>
         </div>
       </div>
-      <div className="flex">
+      <div className="flex mb-10">
         <button
           className="flex-grow py-3 text-brand-green border-b-2 border-b-brand-green"
           onClick={() => setIsSupplying(true)}
@@ -39,7 +39,35 @@ export default function DepositFlow({ closeModal, row }: Props) {
         </button>
       </div>
       <div className="p-6" style={{ background: "#1C1E22" }}>
-        DEFI!
+        <div className="flex">
+          <span className="font-bold mr-3">Supply Rates</span>{" "}
+          <img src="/images/box-arrow.svg" alt="box arrow" />
+        </div>
+        <div className="flex items-center">
+          <div>
+            <img src="/images/supply-icon.svg" />
+          </div>
+          <div>Supply APY</div>
+          <div>X.XX%</div>
+        </div>
+        <div className="flex items-center">
+          <div>
+            <img src="/images/distribution-icon.svg" />
+          </div>
+          <div>Distribution APY</div>
+          <div>0%</div>
+        </div>
+
+        <div className="mb-8">
+          <button className="py-4 text-center text-white font-bold rounded bg-brand-green w-full">
+            Enable
+          </button>
+        </div>
+
+        <div className="flex text-gray-500">
+          <div className="flex-grow">Wallet Balance</div>
+          <div>0 {row.name}</div>
+        </div>
       </div>
     </div>
   ) : (
