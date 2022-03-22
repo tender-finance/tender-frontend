37ac63bfaa46698129397ca2e04932b38ffaf6ff
Ryan LaBouve <imryanlabouve@gmail.com>
2022-03-22 14:28:37 -0500

    First pass at withdraw

--- app/components/deposit-flow.tsx
+++ app/components/deposit-flow.tsx
@@ -55,6 +55,29 @@ async function deposit(value: string, signer: Signer, cToken: cToken) {
   let tx = await contract.mint(formattedValue);
   // }
 }
+async function redeem(value: string, signer: Signer, cToken: cToken) {
+  // if (isCEth) {
+  //   console.log("redeem() with cEth");
+
+  //   const formattedValue = ethers.utils.parseEther(value);
+  //   console.log("input value:", value, "formattedValue:", formattedValue);
+
+  //   let contract = new ethers.Contract(
+  //     address,
+  //     sampleAbi,
+  //     web3React.library?.getSigner()
+  //   );
+  //   let tx = await contract.redeemUnderlying(formattedValue);
+  // } else {
+  console.log("redeem() with cToken", name, "address:", cToken.address);
+
+  const formattedValue = ethers.utils.parseEther(value);
+  console.log("input value:", value, "formattedValue:", formattedValue);
+
+  let contract = new ethers.Contract(cToken.address, SampleCTokenAbi, signer);
+  let tx = await contract.redeem(formattedValue);
+  // }
+}
 
 export default function DepositFlow({ closeModal, row }: Props) {
   let [isSupplying, setIsSupplying] = useState<boolean>(true);
@@ -209,6 +232,26 @@ export default function DepositFlow({ closeModal, row }: Props) {
             </div>
             <div>Deposit {row.name}</div>
           </div>
+
+          {!isEnabled && (
+            <div>
+              <div className="mt-12 mb-6 bg-white w-16 h-16 rounded-full ml-auto mr-auto"></div>
+              <div className="max-w-sm text-center m-auto mt-5 mb-5 text-sm text-gray-400">
+                To supply or repay {row.name} to the Tender Protocol, you need
+                to enable it first.
+              </div>
+            </div>
+          )}
+          {isEnabled && (
+            <div className="flex flex-col justify-center items-center overflow-hidden">
+              <input
+                onChange={(e) => setValue(e.target.value)}
+                className="bg-transparent text-6xl text-white text-center outline-none"
+                placeholder="0"
+              />
+              <div className="text-gray-400 text-sm m-auto">Max ⬆</div>
+            </div>
+          )}
         </div>
         <div className="flex">
           <button
@@ -224,8 +267,69 @@ export default function DepositFlow({ closeModal, row }: Props) {
             Withdraw
           </button>
         </div>
-        <div className="p-6" style={{ background: "#1C1E22" }}>
-          DEFI!
+        <div className="py-8" style={{ background: "#1C1E22" }}>
+          <div className="py-6 px-12" style={{ background: "#1C1E22" }}>
+            <div className="flex mb-4">
+              <span className="font-bold mr-3">Supply Rates</span>{" "}
+              <img src="/images/box-arrow.svg" alt="box arrow" />
+            </div>
+            <div className="flex items-center mb-3 text-gray-400 border-b border-b-gray-600 pb-6">
+              <div className="mr-3">
+                <img src="/images/supply-icon.svg" />
+              </div>
+              <div className="flex-grow">Borrow APY</div>
+              <div>X.XX%</div>
+            </div>
+            <div className="flex items-center text-gray-400 pt-4 pb-8">
+              <div className="mr-3">
+                <img src="/images/distribution-icon.svg" />
+              </div>
+              <div className="flex-grow">Distribution APY</div>
+              <div>0%</div>
+            </div>
+
+            <div className="mb-8">
+              {!signer && <div>Connect wallet to get started</div>}
+              {signer && !isEnabled && (
+                <button
+                  onClick={async () => {
+                    try {
+                      // @ts-ignore existence of signer is gated above.
+                      await enable(signer, row.token, row.cToken);
+                      setIsEnabled(true);
+                    } catch (e) {
+                      console.error(e);
+                    }
+                  }}
+                  className="py-4 text-center text-white font-bold rounded bg-brand-green w-full"
+                >
+                  Enable
+                </button>
+              )}
+
+              {signer && isEnabled && (
+                <button
+                  onClick={async () => {
+                    try {
+                      // TODO: error state no value
+                      // @ts-ignore existence of signer is gated above.
+                      redeem(value, signer, row.cToken);
+                    } catch (e) {
+                      console.error(e);
+                    }
+                  }}
+                  className="py-4 text-center text-white font-bold rounded bg-brand-green w-full"
+                >
+                  Withdraw
+                </button>
+              )}
+            </div>
+
+            <div className="flex text-gray-500">
+              <div className="flex-grow">Currently Borrowing</div>
+              <div>0 {row.name}</div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
