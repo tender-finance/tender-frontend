23217780fca4b808d50feaf6763df9e60fb49733
Richard Bross <rbross3@gmail.com>
2022-04-15 08:17:38 -0500

    remove redundant add to wallet

--- app/components/swap-table.tsx
+++ app/components/swap-table.tsx
@@ -200,7 +200,6 @@ export default function SwapTable() {
           network not supported. Please check back soon.
         </div>
       )}
-      {!chainId && <ConnectWallet />}
     </div>
   );
 }
