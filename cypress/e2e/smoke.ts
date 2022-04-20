fb1ff8aa8262d939cfc64837b0cc2cd73b70ff90
Ryan LaBouve <imryanlabouve@gmail.com>
2022-04-20 13:58:04 -0500

    Removes unnecessary commands

--- cypress/e2e/smoke.ts
+++ cypress/e2e/smoke.ts
@@ -1,11 +1,4 @@
-import faker from "@faker-js/faker";
-
 describe("smoke tests", () => {
-  afterEach(() => {
-    // TODO: adapt for metamask wallet
-    // cy.cleanupUser();
-  });
-
   it("should allow you to enter app", () => {
     cy.visit("/");
     cy.findByTestId("enter-app").click();
