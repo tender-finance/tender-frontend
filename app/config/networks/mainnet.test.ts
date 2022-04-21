e155c1fffa01578ee7a696e9920229c5772dd690
Ryan LaBouve <imryanlabouve@gmail.com>
2022-04-21 13:24:16 -0500

    Fixes type error on test (#24)

--- app/config/networks/mainnet.test.ts
+++ app/config/networks/mainnet.test.ts
@@ -1,4 +1,4 @@
 import mainnet from "./mainnet";
 test("mainnet has contracts", () => {
-  expect(Object.keys(mainnet.Contracts).length > 0).toBe(true);
+  expect(Object.keys(mainnet.Contracts).length > 0).to.equal(true);
 });
