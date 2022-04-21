106f8c4a48edb8c741096a51348e9ef2a34c412c
Ryan LaBouve <imryanlabouve@gmail.com>
2022-04-21 11:59:50 -0500

    borrow => borrow

--- app/components/borrow-flow.tsx
+++ app/components/borrow-flow.tsx
@@ -98,6 +98,7 @@ export default function BorrowFlow({
       formattedBorrowedAmount={formattedBorrowedAmount}
       signer={signer}
       borrowLimitUsed={borrowLimitUsed}
+      borrowLimit={borrowLimit}
       walletBalance={walletBalance}
     />
   );
