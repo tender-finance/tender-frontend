d3b7d8c161447817acec958805e9f8a1f0d41e01
John Mosesman <johnmosesman@gmail.com>
2022-05-09 16:58:18 -0500

    Example config

---
+++ app/config/networks/example-network.ts
@@ -0,0 +1,23 @@
+export default {
+  Contracts: {
+    Comptroller: "0x0...",
+  },
+  PriceOracles: {
+    DAI: "0x0...",
+  },
+  Tokens: {
+    DAI: {
+      name: "Dai",
+      symbol: "DAI",
+      decimals: 18,
+      address: "0x0...",
+      icon: "/images/coin-icons/dai.svg",
+      cToken: {
+        name: "Compound Dai 📈",
+        symbol: "cDAI",
+        decimals: 8,
+        address: "0x0...",
+      },
+    },
+  },
+};
