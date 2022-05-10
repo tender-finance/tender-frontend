919b5811f936d243f4a21c94a2df7112575ee8ea
John Mosesman <johnmosesman@gmail.com>
2022-05-10 11:00:00 -0500

    More readable token pair creation

--- app/hooks/use-token-pairs.ts
+++ app/hooks/use-token-pairs.ts
@@ -1,38 +1,55 @@
 import { useState, useEffect } from "react";
 import type { NetworkData, Token, cToken, TokenPair } from "~/types/global";
 
+function generateTokenPair(
+  networkData: NetworkData,
+  symbol: string
+): TokenPair {
+  let token: Token = networkData.Tokens[symbol];
+  let cToken: cToken = token.cToken;
+
+  return {
+    token,
+    cToken,
+  };
+}
+
+function validTokenConfigs(
+  networkData: NetworkData,
+  tokenSymbols: string[]
+): string[] {
+  return tokenSymbols.filter((symbol) => {
+    let token: Token = networkData.Tokens[symbol];
+    let cToken: cToken = token.cToken;
+    let priceOracleAddress: string = networkData.PriceOracles[symbol];
+
+    // Useful logs to know when the config isn't right
+    if (!token || !cToken || !priceOracleAddress) {
+      console.error(
+        `Missing token, cToken, or priceOracleAddress in config for ${symbol}.`
+      );
+      console.error("token: ", token);
+      console.error("cToken: ", cToken);
+      console.error("priceOracleAddress: ", priceOracleAddress);
+
+      return false;
+    }
+
+    return true;
+  });
+}
+
 function generateTokenPairs(networkData: NetworkData): TokenPair[] {
   let tokenSymbols: string[] = Object.keys(networkData.Tokens);
 
-  let tokenPairs: (TokenPair | null)[] = tokenSymbols.map(
-    (symbol: string): TokenPair | null => {
-      const tokenData: Token = networkData.Tokens[symbol];
-
-      let token: Token = networkData.Tokens[tokenData.symbol];
-      let cToken: cToken = token.cToken;
-      let priceOracleAddress: string =
-        networkData.PriceOracles[tokenData.symbol];
-
-      // Useful logs to know when the config isn't right
-      if (!token || !cToken || !priceOracleAddress) {
-        console.error(
-          `Missing token, cToken, or priceOracleAddress in config for ${symbol}.`
-        );
-        console.error("token: ", token);
-        console.error("cToken: ", cToken);
-        console.error("priceOracleAddress: ", priceOracleAddress);
-
-        return null;
-      }
-
-      return {
-        token,
-        cToken,
-      };
-    }
+  let validTokenSymbols: string[] = validTokenConfigs(
+    networkData,
+    tokenSymbols
   );
 
-  return tokenPairs.filter((p): p is TokenPair => p !== null);
+  return validTokenSymbols.map((symbol) => {
+    return generateTokenPair(networkData, symbol);
+  });
 }
 
 export function useTokenPairs(
