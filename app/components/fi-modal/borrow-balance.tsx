f2f4d95c85f406a1b164bb08611adb89a8b2f6a5
Ryan LaBouve <imryanlabouve@gmail.com>
2022-05-20 14:01:08 -0500

    Surface nan check to tender.ts (#82)

--- app/components/fi-modal/borrow-balance.tsx
+++ app/components/fi-modal/borrow-balance.tsx
@@ -46,9 +46,7 @@ export default function BorrowBalance(props: BorrowBalanceProps) {
       <div className="flex items-center mb-3 text-gray-400 border-b border-b-gray-600 py-5">
         <div className="flex-grow">Borrow Limit Used</div>
         <div>
-          {(value == "0" || !isValid) && (
-            <>{borrowLimitUsed === "NaN" ? "0" : borrowLimitUsed}%</>
-          )}
+          {(value == "0" || !isValid) && <>{borrowLimitUsed}%</>}
           {isValid && value != "0" && (
             <>
               {borrowLimitUsed}% <span className="text-brand-green">→ </span>
--- app/components/fi-modal/borrow-limit.tsx
+++ app/components/fi-modal/borrow-limit.tsx
@@ -45,9 +45,7 @@ export default function BorrowLimit(props: BorrowLimitProps) {
       <div className="flex items-center mb-3 text-gray-400 border-b border-b-gray-600 py-5">
         <div className="flex-grow">Borrow Limit Used</div>
         <div>
-          {(value == "0" || !isValid) && (
-            <>{borrowLimitUsed === "NaN" ? "0" : borrowLimitUsed}%</>
-          )}
+          {(value == "0" || !isValid) && <>{borrowLimitUsed}%</>}
           {isValid && value != "0" && (
             <>
               {borrowLimitUsed}% <span className="text-brand-green">→ </span>
