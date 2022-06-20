7546c59bd703bd7fa123ad6cbfeb8ddbe7cbdf2a
Eli <eli@unspun.io>
2022-06-20 13:18:04 -0700

    fix error of client code rendering on server

--- app/components/connect-wallet.tsx
+++ app/components/connect-wallet.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { hooks, metaMask } from "~/connectors/meta-mask";
 const { useAccounts, useError, useIsActivating, useIsActive } = hooks;
 
@@ -7,6 +7,7 @@ export default function ConnectWallet() {
   const error = useError();
   const isActivating = useIsActivating();
   const isActive = useIsActive();
+  const [onClient, setOnClient] = useState(false);
 
   function truncateAccount(accounts: string[]): string {
     return accounts
@@ -15,42 +16,52 @@ export default function ConnectWallet() {
   }
 
   useEffect(() => {
+    setOnClient(true)
     void metaMask.connectEagerly();
   }, []);
 
   return (
     <div className="box">
-      {isActive && accounts && (
-        <div className="flex text-sm text-gray-400">
-          <div className=" py-2 px-4">
-            Connected as {truncateAccount(accounts)}
-          </div>
-        </div>
-      )}
-      {typeof window !== "undefined" && !window.ethereum && (
-        <a
-          className="border border-brand-green text-brand-green py-4 px-4 rounded-md text-sm"
-          target="_blank"
-          href="https://metamask.io/"
-        >
-          Install MetaMask to get started
-        </a>
-      )}
-      {typeof window !== "undefined" && window.ethereum && !isActive && (
-        <div className="text-sm">
-          <button
-            data-testid="connect-wallet"
-            style={{
-              background: "linear-gradient(270deg, #1BD6CF 0%, #00E5AF 100%)",
-            }}
-            className="bg-brand-green text-gray-900 py-4 px-4 rounded-md text-sm"
-            onClick={isActivating ? undefined : () => metaMask.activate()}
-            disabled={isActivating}
+      {/* only on the client */}
+      {onClient && <>
+
+        {isActive && accounts && (
+          <span className="flex text-sm text-gray-400">
+            <span className=" py-2 px-4">
+              Connected as {truncateAccount(accounts)}
+            </span>
+          </span>
+        )}
+
+        {/* Prompt to Install Metamask if window.ethereum is not available */}
+        {!window.ethereum && (
+          <a
+            className="border border-brand-green text-brand-green py-4 px-4 rounded-md text-sm"
+            target="_blank"
+            href="https://metamask.io/"
           >
-            Connect Wallet
-          </button>
-        </div>
-      )}
+            Install MetaMask to get started
+          </a>
+        )}
+
+        {/* Prompt to Connect Wallet if not active */}
+        {window.ethereum && !isActive && (
+          <div className="text-sm">
+            <button
+              data-testid="connect-wallet"
+              style={{
+                background: "linear-gradient(270deg, #1BD6CF 0%, #00E5AF 100%)",
+              }}
+              className="bg-brand-green text-gray-900 py-4 px-4 rounded-md text-sm"
+              onClick={isActivating ? undefined : () => metaMask.activate()}
+              disabled={isActivating}
+            >
+              Connect Wallet
+            </button>
+          </div>
+        )}
+      </>}
+
       {error?.message && (
         <div className="text-xs text-gray-500">{error.message}</div>
       )}
